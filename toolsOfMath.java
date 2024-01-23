fabd9d5b84e178de97ef9da9ed8e8457f201fa2f
IshanAgg125 <tuo10683@temple.edu>
2024-01-23 14:01:02 -0500

    firstTest

---
+++ toolsOfMath.java
@@ -0,0 +1,16 @@
+
+
+public class toolsOfMath {
+    int x;
+    int y;
+
+    public toolsOfMath(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public int getMultiplication() {
+        return this.x * this.y;
+    }
+}
+
---
+++ toolsOfMathRunner.java
@@ -0,0 +1,12 @@
+
+public class toolsOfMathRunner {
+
+    public static void main(String[] args) {
+        toolsOfMath tom = new toolsOfMath(6, 7);
+        int x = tom.getMultiplication();
+        System.out.println("answer = "x);
+    }
+
+}
+
+
